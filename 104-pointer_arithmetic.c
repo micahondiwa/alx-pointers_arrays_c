0d6f2a5571a87d914f179001fca69d186bd8b504
Micah Ondiwa <110167699+Mikondiwa@users.noreply.github.com>
2022-09-19 14:27:09 +0300

    Update 104-pointer_arithmetic.c

--- 104-pointer_arithmetic.c
+++ 104-pointer_arithmetic.c
@@ -16,25 +16,13 @@ int main(void)
 	*(a + 4) = 498;
 	printf("Value of a[0]: %d\n", *a);
 	printf("Value of a[1]: %d\n", *(a + 1));
-
-	   printf("Value of a[2]: %d\n", *(a + 2));
-
-	      printf("Value of a[3]: %d\n", *(a + 3));
-
-	         printf("Value of a[4]: %d\n", *(a + 4));
-
-		    printf("----------------\n");
-
-		       printf("Value of 'a' (also address of a[0]): %p\n", a);
-
-		          printf("Address of 'a[1]': %p\n", (a + 1));
-
-			     printf("Address of 'a[1]': %p\n", &(*(a + 1)));
-
-			        return (0);
-
-
-
-
+        printf("Value of a[2]: %d\n", *(a + 2));
+	printf("Value of a[3]: %d\n", *(a + 3));
+	printf("Value of a[4]: %d\n", *(a + 4));
+        printf("----------------\n");
+	printf("Value of 'a' (also address of a[0]): %p\n", a);
+	printf("Address of 'a[1]': %p\n", (a + 1));
+	printf("Address of 'a[1]': %p\n", &(*(a + 1)));
+	return (0);
 }
 
