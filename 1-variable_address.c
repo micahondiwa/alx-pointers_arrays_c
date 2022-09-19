41934db996f5469ca81f402239414a4711a675c5
Mikondiwa <mikondiwa@gmail.com>
2022-09-19 02:55:30 -0700

    Variable addresses

---
+++ 1-variable_address.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - addresses of variables
+ *
+ * Retunr: Always 0.
+ */
+int main(void)
+{
+	char c;
+	int n;
+
+	printf("Address of variable 'c': %p\n", %c);
+	printf("Adress of variable 'n': %p\n", &n);
+	return (0);
+}
