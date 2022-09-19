12629201810bd88ba3e5e4bb4a8e569fa71e49c6
Mikondiwa <mikondiwa@gmail.com>
2022-09-19 04:11:36 -0700

    Array size

---
+++ 103-array_size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printing the size of an array
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int array[10];
+
+	printf("Sizeof array: %lu\n", sizeof(array));
+	return (0);
+}
+
