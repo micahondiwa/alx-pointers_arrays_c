2ff52ff489aaa1091b46ab024c035873f4164b0a
Mikondiwa <mikondiwa@gmail.com>
2022-09-19 04:02:49 -0700

    Modify a

---
+++ 101-modify_array.c
@@ -0,0 +1,17 @@
+#include<stdio.c>
+/**
+ * main - You can modify a
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a[5];
+	int b;
+	int c[5];
+
+	a = 0; /* nop*/
+	a = &b; /* nop*/
+	a = c; /* nop*/
+	return (0);
+}
